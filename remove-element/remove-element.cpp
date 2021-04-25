b911c6bbe2d8182e4e9b36069cc6feb6265d46d3
VidurAgarwal25 <81953841+VidurAgarwal25@users.noreply.github.com>
2021-04-25 20:08:28 +0530

    Time: 4 ms (45.88%), Space: 8.8 MB (18.34%) - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==val){
+                nums[i]=INT_MAX;
+                n--;
+            }
+        }
+        sort(nums.begin(),nums.end());
+        return n;
+    }
+};
