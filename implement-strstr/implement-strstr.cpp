93c6208877aa169ef5a3a2a384db5557aee9c702
VidurAgarwal25 <81953841+VidurAgarwal25@users.noreply.github.com>
2021-04-25 19:49:56 +0530

    Time: 4 ms (75.43%), Space: 6.9 MB (21.15%) - LeetHub

---
+++ implement-strstr/implement-strstr.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int l1=haystack.length();
+        int l2=needle.length();
+        
+        for(int i=0;i<=l1-l2;i++){
+            if(haystack.substr(i,l2)==needle)
+                return i;
+        }
+        return -1;
+    }
+};
