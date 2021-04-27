16f4d156a2a3e29926c4a60f6e86e23e80212651
VidurAgarwal25 <81953841+VidurAgarwal25@users.noreply.github.com>
2021-04-27 12:11:40 +0530

    Time: 8 ms (60.23%), Space: 10.7 MB (24.50%) - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -4,19 +4,13 @@ public:
         unordered_map<int,int>m;
         int x=0;
         for(int i=0;i<arr.size();i++){
-            m[2*arr[i]]=arr[i];
-            if(arr[i]==0)
-                x++;
-        }
-        if(x>=2)
-            return true;
-        else{
             
-        for(int i=0;i<arr.size();i++){
-            
-            if(m[arr[i]])
+            if(m[arr[i]*2])
                 return true;
-            }
+            if(arr[i]%2==0&&m[arr[i]/2])
+                return true;
+            
+            m[arr[i]]=1;
         }
         return false;
     }
