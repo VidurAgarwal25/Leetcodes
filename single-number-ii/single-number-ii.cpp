8a13c9c26a5a6e77f95ffc551794e4e061371679
VidurAgarwal25 <81953841+VidurAgarwal25@users.noreply.github.com>
2021-07-08 18:30:59 +0530

    Time: 8 ms (80.97%), Space: 10.1 MB (9.53%) - LeetHub

--- single-number-ii/single-number-ii.cpp
+++ single-number-ii/single-number-ii.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int x;
-        if(nums.size()<3)
-            return nums[0];
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-2;i+=3){
-            if(nums[i]!=nums[i+1]||nums[i]!=nums[i+2]||nums[i+1]!=nums[i+2])
-            {
-                x=nums[i];
-                break;
-            }
+        unordered_map<int ,int>map;
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
         }
-        if(nums[nums.size()-1]!=nums[nums.size()-2]||nums[nums.size()-2]!=nums[nums.size()-3]||nums[nums.size()-1]!=nums[nums.size()-3])
-            x=nums[nums.size()-1];
+        int x;
+        for(auto i=map.begin();i!=map.end();i++){
+            if(i->second==1)
+                x=i->first;
+        }
         return x;
     }
 };
