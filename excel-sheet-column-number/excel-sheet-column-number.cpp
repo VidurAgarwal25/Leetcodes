bee65d889a8229a92a4bb305d8aa4037c64c4a55
VidurAgarwal25 <81953841+VidurAgarwal25@users.noreply.github.com>
2021-07-14 12:27:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (8.76%) - LeetHub

--- excel-sheet-column-number/excel-sheet-column-number.cpp
+++ excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    int titleToNumber(string s) {
-        int n=s.length();
-        int m=0,i=0;
-        while(i<n)
-        m=(m*26)+(s[i++]-64);
-        return m;
+    int titleToNumber(string ct) {
+        reverse(ct.begin(),ct.end());
+        int len=ct.length();
+        int p=0,x,s=0;
+        for(int i=0;i<len;i++){
+            x=ct[i]-64;
+            s+=x*pow(26,p);
+            p++;
+        }
+        return s;
     }
 };
