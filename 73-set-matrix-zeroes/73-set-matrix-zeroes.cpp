1cb879e03b65604dbc7acff964bc1c76bcf39a58
VidurAgarwal25 <81953841+VidurAgarwal25@users.noreply.github.com>
2022-02-16 16:35:17 +0530

    Time: 23 ms (39.73%), Space: 13.2 MB (54.29%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,28 +3,35 @@ public:
     void setZeroes(vector<vector<int>>& a) {
         int m=a.size();
         int n=a[0].size();
+        int r=0,c=0;
+        for(int i=0;i<n;i++){
+            if(a[0][i]==0)
+                r=1;
+        }
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(a[i][0]==0)
+                c=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(a[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        if(a[i][k]!=0){
-                            a[i][k]=-9999;
-                        }
-                    }
-                    
-                    for(int k=0;k<m;k++){
-                        if(a[k][j]!=0){
-                            a[k][j]=-9999;
-                        }
-                    }
+                    a[i][0]=0;
+                    a[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(a[i][j]==-9999)
+      
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(a[i][0]==0||a[0][j]==0)
                     a[i][j]=0;
             }
         }
-    }
+        if(r==1)
+            for(int i=0;i<n;i++)
+                a[0][i]=0;
+        if(c==1)
+            for(int i=0;i<m;i++)
+                a[i][0]=0;
+    }   
 };
