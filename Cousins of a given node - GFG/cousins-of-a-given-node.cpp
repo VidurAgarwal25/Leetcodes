85eaaf7fa12e5d4a4e65652e415de24810336595
VidurAgarwal25 <81953841+VidurAgarwal25@users.noreply.github.com>
2022-03-26 17:58:58 +0530

    Added solution - LeetHub

---
+++ Cousins of a given node - GFG/cousins-of-a-given-node.cpp
@@ -0,0 +1,207 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Binary Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+Node* pointer(Node* root, int n)
+{
+	if (root == NULL)
+		return NULL;
+
+	if (root->data == n)
+		return root;
+
+	Node* l = pointer(root->left, n);
+	if (l != NULL && l->data == n)
+		return l;
+
+	Node* r = pointer(root->right, n);
+	return r;
+
+}
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+// A Binary Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};*/
+
+class Solution
+{
+    public:
+    vector<int> printCousins(Node* r, Node* b)
+    {
+        //code here
+        queue<pair<Node*,Node*>>q;
+        vector<int>res;
+        
+        if(r==NULL)
+        return res;
+        
+        if(r==b)
+        {
+            res.push_back(-1);
+            return res;
+        }
+        
+        q.push({r,NULL});
+        int f=0;Node*t;
+        while(!q.empty()){
+            if(f==1)
+            break;
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                pair<Node*,Node*>temp=q.front();
+                q.pop();
+                if((temp.first)->left!=NULL)
+                q.push({temp.first->left,temp.first});
+                if(temp.first->right!=NULL)
+                q.push({temp.first->right,temp.first});
+                if(temp.first->left==b||temp.first->right==b)
+                {
+                    f=1;
+                    t=temp.first;
+                }
+            }
+        }
+        while(!q.empty()){
+            pair<Node*,Node*>temp=q.front();
+            q.pop();
+            if(temp.second!=t)
+            res.push_back(temp.first->data);
+        }
+        if(res.size()==0){
+            res.push_back(-1);
+        }
+      
+        return res;
+    }
+    
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin >> t;
+	getchar();
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		getchar();
+
+		string inp;
+		getline(cin, inp);
+		
+		struct Node* root = buildTree(inp);
+
+		Node* p = pointer(root, n);
+
+        Solution ob;
+		vector<int> ans = ob.printCousins(root, p);
+
+		for (int i = 0; i < ans.size(); i++)
+			cout << ans[i] << " ";
+
+		cout << "\n";
+
+
+	}
+
+	return 0;
+}
+
+  // } Driver Code Ends
