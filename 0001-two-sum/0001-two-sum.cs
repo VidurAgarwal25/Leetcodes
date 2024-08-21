73c916102d95e7c3c53d9e8e2bb25e1be2438ded
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2024-08-21 10:40:48 +0530

    Time: 118 ms (71.67%), Space: 48.5 MB (9.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,22 +1,20 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> indexing = new Dictionary<int, int>();
-        List<int> res = new List<int>();
+        Dictionary<int, int> indexing = new Dictionary<int, int>(nums.Length); // Initialize with capacity
         
         for (int i = 0; i < nums.Length; i++) {
             int check = target - nums[i];
             
-            if (indexing.ContainsKey(check)) {
-                res.Add(indexing[check]);
-                res.Add(i);
-                break;  // You can exit the loop once the solution is found
+            if (indexing.TryGetValue(check, out int index)) {
+                return new int[] { index, i };
             }
-            else {
+            
+            // Only add to the dictionary if the key does not exist
+            if (!indexing.ContainsKey(nums[i])) {
                 indexing[nums[i]] = i;
             }
         }
         
-        return res.ToArray();
-    
+        return null; // In case no solution is found    
     }
 }
